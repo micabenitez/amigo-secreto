02e427ae12960479e70b999c440ebc5caf16ee15
Micaela Benitez <mabenittez@gmail.com>
2025-07-28 17:15:10 -0300

    Add agregarAmigo, sortearAmigo and mostrarAmigos functions

--- app.js
+++ app.js
@@ -1,1 +1,42 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    let nombre = document.getElementById('amigo').value;
+    if (nombre === '') {
+        alert('Por favor, inserte un nombre.');
+        return;
+    }
+    amigos.push(nombre);
+    limpiarInput();
+    mostrarAmigos();
+}
+
+function limpiarInput() {
+    document.getElementById('amigo').value = '';
+}
+
+function sortearAmigo() {
+    if(amigos-length === 0){
+        alert('No hay amigos para sortear. Por favor, agregue al menos un amigo.');
+        return;
+    }
+    let amigo = amigos[Math.floor(Math.random() * amigos.length)];
+    let resultado = document.getElementById('resultado');
+    resultado.textContent = `El amigo sorteado es: ${amigo}`;
+}
+
+function mostrarAmigos() {
+    let lista =  document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+    
+    for (let i = 0; i < amigos.length; i++) {
+        let listItem = document.createElement('li');
+        listItem.textContent = amigos[i];
+        lista.appendChild(listItem);
+    }
+}
+
+
+
+
+
